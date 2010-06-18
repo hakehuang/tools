cf5a85fd79266b71999bbd04d605e7e17c2158f3
Hake Huang <b20222@freescale.com>
2010-06-18 16:40:08 +0800

    fix the xml parse loop error
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- docbook/xml_parser.py
+++ docbook/xml_parser.py
@@ -5,15 +5,21 @@ import sys
 
 #virables
 ori_array = [ ]
+ori_end_array = [ ]
 target_array = [ ]
+target_end_array = [ ]
 seq = 0
+seq_end = 0
 add_content = 0
-
 # 3 store the  
 def store_ori_start_element(name, attrs):
 	ori_array.append(name);
+def store_ori_end_element(name):
+	ori_end_array.append(name);
 def store_target_start_element(name, attrs):
 	target_array.append(name);
+def store_target_end_element(name):
+	target_end_array.append(name);
 # 3 handler functions
 def start_element(name, attrs):
 	global add_content
@@ -22,30 +28,49 @@ def start_element(name, attrs):
 	global ori_array
 	global target_array
 	global f
-	add_content = 0
-	print 'Start element:', name, attrs
-	if( seq < len_ori and len(ori_array) > 1):
-		if (name == ori_array[seq] or "skip" == ori_array[seq]):
-			if( name == ori_array[seq]):
-				f.write("\n<")
-				f.write(target_array[seq]);
-				f.write(">")
-				add_content = 1
+	print 'Start element:', name, attrs, seq
+	if (name == ori_array[seq]):
+		if( "skip" == target_array[seq]):
+			add_content = 0
+		elif ("char" == target_array[seq]):
+			add_content = 1
+		else:
+			f.write("\n<")
+			f.write(target_array[seq]);
+			f.write(">")
+			add_content = 1
+		if(seq < len_ori - 1):
 			seq = seq + 1
 	else:
-		seq = 0	
+		#abnormal happen
+		add_content = 0
+		print 'abnormal!!!', name , ori_array[seq], seq
+		return
 def end_element(name):
 	global add_content
+	global seq_end
 	global seq
-	global len_ori
-	global ori_array
-	global target_array
+	global ori_end_array
+	global target_end_array
 	global f
 	print 'End element:', name
-	if (add_content == 1 and len(target_array) > 1 ):
-		f.write("\n<")
-		f.write(target_array[seq - 1])
-		f.write(">")
+	if (seq_end >= len(ori_end_array)):
+		seq_end = 0
+		seq = 0
+	if (name == ori_end_array[seq_end] and  seq > 0):
+		if(target_end_array[seq_end] == "char" ):
+			pass
+		elif("skip" == target_end_array[seq_end]):
+			pass
+		else:
+			f.write("\n<\\")
+			f.write(target_end_array[seq_end])
+			f.write(">")
+		seq_end = seq_end + 1
+	else:
+		#abnormal happen
+		print 'end abnormal!!!', name , ori_end_array[seq_end], seq_end
+		return
 def char_data(data):
 	global add_content
 	global seq
@@ -63,6 +88,8 @@ def char_data(data):
 	if(add_content == 1):
 		f.write("\n")
 		f.write(repr(data)[2:tl-1].replace("\\t",""))
+	else:
+		return
 # Script starts from here
 if len(sys.argv) < 2:
 	print 'No action specified.'
@@ -84,21 +111,25 @@ Options include:
 else:
 	p1 = xml.parsers.expat.ParserCreate()
 	p1.StartElementHandler = store_ori_start_element
+	p1.EndElementHandler = store_ori_end_element
 	try:
 		rFile = open("ori_file",'r')
 		p1.ParseFile(rFile)
 	finally:
 		rFile.close()
 	print 'current ori array is:',ori_array
+	print 'current ori_end array is:',ori_end_array
 	len_ori = len(ori_array) 
 	p2 = xml.parsers.expat.ParserCreate()
 	p2.StartElementHandler = store_target_start_element
+	p2.EndElementHandler =  store_target_end_element
 	try:
 		tFile = open("target_file",'r')
 		p2.ParseFile(tFile)
 	finally:
 		tFile.close()
 	print 'current target array is:',target_array
+	print 'current target_end array is:',target_end_array
 	len_target = len(target_array) 
 	f = file('db.xml', 'w')
 	p3 = xml.parsers.expat.ParserCreate()
